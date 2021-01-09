07149d6c60c73751b9b429cbcc31579bc0c33e70
karim107-git <karim107@yahoo.fr>
2021-01-08 22:31:33 -0500

    ajout d'une fonction addition

---
+++ Exercice1.js
@@ -0,0 +1,18 @@
+//fonction qui additionne eux nombres
+function somme(x,y){
+    return x+y;
+}
+//affichage du resultat
+console.log(somme(100,50));  //Affiche  150
+console.log(somme(-10,10));  //Affiche 0
+console.log(somme(2,3));     //Affiche 5
+console.log(somme(5.2,-7.5)); //Affiche -2.3
+console.log(somme(1,70)); //Affiche 71
+console.log(somme(0,0));  //Affiche 0
+
+
+
+
+
+
+
